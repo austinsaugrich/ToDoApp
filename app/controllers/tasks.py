73219d1902da8bccf8c20fe1eec5d2d1cb54fad9
austinsaugrich <asaugrich19@gmail.com>
2024-02-05 00:18:31 -0600

    ToDoApp

---
+++ app/controllers/tasks.py
@@ -0,0 +1,121 @@
+from app import app
+from flask import Flask, session, render_template, request, redirect
+from app.models.task import Task
+
+Tasks = [
+    {
+        'id': 3423,
+        'name': 'Shopping',
+        'urgency': 1,
+        'date': 'today'
+    }
+]
+
+
+def pull_task_by_id(id):
+    found_task = None
+    for task in session['tasks']:
+        if task['id'] == id:
+            found_task = task
+    return found_task
+
+# clear session data
+
+
+@app.route('/')
+def clearses():
+    # session.clear()
+    # return 'session cleared!'
+    return redirect('/dashboard')
+
+# new task form
+
+
+@app.route('/add/task')
+def add_task_form():
+    # if 'tasks' not in session:
+    # session['tasks'] = Tasks
+
+    return render_template('add.html')
+
+# requesting data from the form
+
+
+@app.route('/add/task', methods=['POST'])
+def add_task():
+    # form = request.form
+    Task.add(request.form)
+# used for session, switched to db
+
+    # tasks = session['tasks']
+    # tasks.append({
+    # 'id': len(tasks) + 1,
+    # 'name': form['name'],
+    # 'urgency': form['urgency'],
+    # 'date': form['date'],
+    # })
+    # session['tasks'] = tasks
+    return redirect('/dashboard')
+
+
+# update a task
+
+@app.route('/task/edit/<int:id>')
+def update_task_form(id):
+    # if 'tasks' not in session:
+    # session['tasks'] = Tasks
+
+    return render_template('update.html', task=Task.get_by_id(id))
+
+
+@app.route('/task/edit', methods=['POST'])
+def update_task():
+    # form = request.form
+    Task.edit(request.form)
+# no longer using session, using db
+    # task_to_update = pull_task_by_id(int(form['id']))
+    # updated_tasks = []
+    # for task in session['tasks']:
+    # if int(task['id']) == int(task_to_update['id']):
+    # updated_tasks.append({
+    # 'id': task['id'],
+    # 'name': form['name'],
+    # 'urgency': form['urgency'],
+    # 'date': form['date'],
+    # })
+    # else:
+    # updated_tasks.append(task)
+
+    # session['tasks'] = updated_tasks
+    return redirect('/dashboard')
+# view indiviual task
+
+
+@app.route('/task/<int:id>')
+def get_task(id):
+    # if 'tasks' not in session:
+    # session['tasks'] = Tasks
+
+    return render_template('view.html', task=Task.get_by_id(id))
+
+
+# delete a task
+
+@app.route('/task/delete/<int:id>')
+def delete_task(id):
+    # if 'tasks' not in session:
+    # session['tasks'] = Tasks
+    Task.delete(id)
+    # list comprehension
+    # session['tasks'] = [task for task in session['tasks'] if task['id'] != id]
+    return redirect('/dashboard')
+
+
+# shows the todo list
+
+@app.route('/dashboard')
+def dashboard():
+    # if 'tasks' not in session:
+    # session['tasks'] = Tasks
+
+    return render_template('dashboard.html', tasks=Task.get_all())
---
+++ app/models/task.py
@@ -0,0 +1,52 @@
+from app.config.mysqlconnection import MySQLConnection
+
+
+class Task:
+    def __init__(self, data):  # all data is a dic
+        self.id = data['id']
+        self.name = data['name']
+        self.urgency = data['urgency']
+        self.date = data['date']
+
+    @classmethod
+    def add(cls, data):
+        query = """
+        INSERT INTO tasks(name, urgency, date)
+VALUES(%(name)s, %(urgency)s, %(date)s);
+        """
+
+        return MySQLConnection('tasks').query_db(query, data)
+
+    @classmethod
+    def edit(cls, data):
+        query = """
+        UPDATE tasks SET name = %(name)s, urgency = %(urgency)s, date = %(date)s
+WHERE tasks.id = %(id)s
+        """
+        return MySQLConnection('tasks').query_db(query, data)
+
+    @classmethod
+    def delete(cls, id):
+        query = """
+        DELETE FROM tasks WHERE tasks.id = %(id)s
+        """
+        return MySQLConnection('tasks').query_db(query, {'id': id})
+
+    @classmethod
+    def get_all(cls):
+        query = 'SELECT * FROM tasks;'
+
+        results = MySQLConnection('tasks').query_db(query)
+
+        all_tasks = []
+        for row in results:
+            all_tasks.append(Task(row))
+        return all_tasks
+
+    @classmethod
+    def get_by_id(cls, id):
+        query = 'SELECT * from tasks WHERE tasks.id = %(id)s;'
+
+        results = MySQLConnection('tasks').query_db(query, {'id': id})
+
+        return Task(results[0])
